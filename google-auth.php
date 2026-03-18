3e29534fe97bfee755e1a460aaa041692629a61c
Koenraad <koenraad.descheemaeker@gmail.com>
2026-03-18 19:55:21 +0100

    fix: added select_account to prompt to resolve sticky session issues

--- google-auth.php
+++ google-auth.php
@@ -1,21 +1,20 @@
 <?php
 /**
- * google-auth.php - De 'Dwang-modus' Editie
+ * google-auth.php - De 'Account-Dwang' Editie
  */
 require_once 'config.php';
 
 $params = [
-    'client_id'             => $googleClientID,
-    'redirect_uri'          => $googleRedirectUri,
-    'response_type'         => 'code',
-    'scope'                 => 'https://www.googleapis.com/auth/photoslibrary.readonly',
-    'access_type'           => 'offline',
-    // --- DIT IS HET BREEKIJZER ---
-    'prompt'                => 'consent', // Dwingt Google om de vinkjes ALTIJD te tonen
-    'include_granted_scopes' => 'true'     // Voegt nieuwe rechten toe aan eventuele oude
+    'client_id'              => $googleClientID,
+    'redirect_uri'           => $googleRedirectUri,
+    'response_type'          => 'code',
+    'scope'                  => 'https://www.googleapis.com/auth/photoslibrary.readonly',
+    'access_type'            => 'offline',
+    // We voegen 'select_account' toe aan de prompt
+    'prompt'                 => 'select_account consent', 
+    'include_granted_scopes' => 'true'
 ];
 
 $url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_query($params);
-
 header('Location: ' . $url);
 exit;
