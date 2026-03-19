fd40a305a8f65bf66034d654f9390f0de609a0bf
Koenraad <koenraad.descheemaeker@gmail.com>
2026-03-19 15:10:14 +0100

    fix: final sync of oauth flow after complete account and database reset

--- google-auth.php
+++ google-auth.php
@@ -2,12 +2,13 @@
 require_once 'config.php';
 
 $params = [
-    'client_id'     => trim($googleClientID),
-    'redirect_uri'  => trim($googleRedirectUri),
-    'response_type' => 'code',
-    'scope'         => 'https://www.googleapis.com/auth/photoslibrary.readonly',
-    'access_type'   => 'offline',
-    'prompt'        => 'consent select_account' // DIT MOET DE VINKJES FORCEREN IN TESTING MODE
+    'client_id'              => trim($googleClientID),
+    'redirect_uri'           => trim($googleRedirectUri),
+    'response_type'          => 'code',
+    'scope'                  => 'https://www.googleapis.com/auth/photoslibrary.readonly',
+    'access_type'            => 'offline',
+    'prompt'                 => 'consent select_account', // DIT MOET DE VINKJES FORCEREN
+    'include_granted_scopes' => 'false'
 ];
 
 $url = "https://accounts.google.com/o/oauth2/v2/auth?" . http_build_query($params);
