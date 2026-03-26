825816b3ad6fd605fd416c9c769f5d4de746822d
Koenraad <koenraad.descheemaeker@gmail.com>
2026-03-26 17:26:45 +0100

    refactor: downgrade to readonly scope to bypass 403 security block

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
 <?php
-/** FORCEKES - login.php (Full Scope Power) */
+/** FORCEKES - login.php (Read-Only Mode) */
 require_once 'config.php';
 $params = [
     'client_id'              => $googleConfig['client_id'],
     'redirect_uri'           => $googleConfig['redirect_uri'],
     'response_type'          => 'code',
-    'scope'                  => 'openid email https://www.googleapis.com/auth/photoslibrary',
+    // We gebruiken nu de .readonly scope
+    'scope'                  => 'openid email https://www.googleapis.com/auth/photoslibrary.readonly',
     'access_type'            => 'offline',
     'prompt'                 => 'consent',
     'include_granted_scopes' => 'true'
