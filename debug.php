aab7cec62185e109ef987995d3eb6ad4b9ce8df8
Koenraad <koenraad.descheemaeker@gmail.com>
2026-04-02 10:48:46 +0200

    chore: major cleanup of unused files and legacy scripts

--- debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-echo "<h1>Forcekes Debug Mode</h1>";
-echo "<p>Sessie Email: " . ($_SESSION['user_email'] ?? 'NIET INGESTELD') . "</p>";
-echo "<p>Browser Cookie: " . (isset($_COOKIE['sb-access-token']) ? 'AANWEZIG' : 'ONTBREEKT') . "</p>";
-echo "<p>Admin Check resultaat: " . (($_SESSION['user_email'] ?? '') === 'koen@lauwe.com' ? 'SUCCES' : 'GEWEIGERD') . "</p>";
-echo "<hr>";
-echo "<pre>"; print_r($_SESSION); echo "</pre>";
