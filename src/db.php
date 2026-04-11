46cd986d3779f556d25751d1b2deb8887ef7a2af
Koenraad <koenraad.descheemaeker@gmail.com>
2026-04-11 09:03:52 +0200

    switch to supabase API (remove PDO)

--- src/db.php
+++ src/db.php
@@ -6,6 +6,5 @@ function supabase($table) {
         "Authorization: Bearer " . getenv('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3MzQ4MzM2MCwiZXhwIjo0OTI5MTU2OTYwLCJyb2xlIjoiYW5vbiJ9.LXIJo7fsXhJIQsSi2jIfoqrwV8axI57_6B733vKwCXs'),
         "Content-Type: application/json"
     ];
-
     return [$url, $headers];
 }
